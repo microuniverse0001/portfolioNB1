b7f0a9e1fd198654f07fb14a296882c378961fd5
Nataliia Blashchuk <97595247+microuniverse0001@users.noreply.github.com>
2024-01-07 17:24:56 +0100

    added

---
+++ main.js
@@ -0,0 +1,211 @@
+//  Navbar Scroll Js
+$(window).scroll(function () {
+  $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+
+// Navbar Scroll Spy Js
+$(document).ready(function () {
+  var sectionIds = $('a.nav-link');
+  $(document).scroll(function () {
+    sectionIds.each(function () {
+      var container = $(this).attr('href');
+      var containerOffset = $(container).offset().top;
+      var containerHeight = $(container).outerHeight();
+      var containerBottom = containerOffset + containerHeight;
+      var scrollPosition = $(document).scrollTop();
+      if (scrollPosition < containerBottom - 350 && scrollPosition >= containerOffset - 350) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+  });
+});
+
+
+// Navbar Link Js
+$('.nav-link').on('click', function () {
+  $('.nav-link').removeClass('active');
+  $(this).addClass('active');
+});
+
+$('.nav-link, .vvd ').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+});
+
+
+// Skill Slider Js
+$('.skill-slider').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: ["<img src='arrow1.svg'>", "<img src='arrow2.svg'>"],
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    575: {
+      items: 2
+    },
+    768: {
+      items: 3
+    }
+  }
+});
+
+// Books Slider Js
+$('.books-slider').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: ["<img src='arrow1.svg'>", "<img src='arrow2.svg'>"],
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    575: {
+      items: 2
+    },
+    768: {
+      items: 3
+    }
+  }
+});
+
+// Text Animate JS
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+
+
+
+// WOW Animation JS
+wow = new WOW(
+  {
+    boxClass: 'wow',      // default
+    animateClass: 'animated', // change this if you are not using animate.css
+    offset: 0,          // default
+    mobile: true,       // keep it on mobile
+    live: true        // track if element updates
+  }
+)
+wow.init();
+
+function saveCurrentPage() {
+  document.cookie = "previousPage=" + window.location.href + "; path=/";
+}
+function goBackToPreviousPage1() {
+  const previousPage = getCookie("PreviousPage");
+  if (previousPage) {
+    window.location.href = previousPage;
+  }
+  else {
+    window.location.href = "https://www.behance.net/gallery/180768759/MVP-service-for-bank-employees-to-execute-mandates"
+  }
+}
+function goBackToPreviousPage2() {
+  const previousPage = getCookie("PreviousPage");
+  if (previousPage) {
+    window.location.href = previousPage;
+  }
+  else {
+    window.location.href = "https://www.behance.net/gallery/180747615/Pet-project-Concept-of-the-app-Reservato"
+  }
+}
+function goBackToPreviousPage3() {
+  const previousPage = getCookie("PreviousPage");
+  if (previousPage) {
+    window.location.href = previousPage;
+  }
+  else {
+    window.location.href = "https://www.behance.net/gallery/180748959/Landing-page-for-Kotyan-Basyan"
+  }
+}
+function goBackToPreviousPage4() {
+  const previousPage = getCookie("PreviousPage");
+  if (previousPage) {
+    window.location.href = previousPage;
+  }
+  else {
+    window.location.href = "https://www.behance.net/gallery/180750085/UI-shotsLanding-concept"
+  }
+}
+
+function goBackToPreviousPage5() {
+  const previousPage = getCookie("PreviousPage");
+  if (previousPage) {
+    window.location.href = previousPage;
+  }
+  else {
+    window.location.href = "resume.html"
+  }
+}
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const cookieParts = cookie.split('=');
+    const cookieName = cookieParts[0].trim();
+    if (cookieName === name) {
+      return cookieParts[1];
+    }
+  }
+  return null;
+}
+
